5b8a56ab32a04a3c65b6d1edf3a871cf4832b9c5
Mehdi4924 <85990825+Mehdi4924@users.noreply.github.com>
2022-11-08 11:58:00 -0800

    fdsaa

--- Src/Navigation/AppFlow.js
+++ Src/Navigation/AppFlow.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Inventories from '../Screens/AppFlow/Inventories';
-import Classified from '../Screens/AppFlow/Classified';
 
 export default function AppFlow() {
   const MainFlow = createNativeStackNavigator();
   return (
     <MainFlow.Navigator screenOptions={{headerShown: false}}>
-      <MainFlow.Screen name="Classified" component={Classified} />
-      <MainFlow.Screen name="Inventories" component={Inventories} />
     </MainFlow.Navigator>
   );
 }
--- Src/Navigation/BottomNavigator.js
+++ Src/Navigation/BottomNavigator.js
@@ -1,6 +1,8 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
+import Classified from '../Screens/BottomTab/Classified';
 import HomeScreen from '../Screens/BottomTab/HomeScreen';
+import Inventories from '../Screens/BottomTab/Inventories';
 
 const Tab = createBottomTabNavigator();
 
@@ -8,6 +10,8 @@ export default function BottomNavigator() {
         return (
           <Tab.Navigator screenOptions={{headerShown:false}}>
             <Tab.Screen name="HomeScreen" component={HomeScreen} />
+            <Tab.Screen name="Classified" component={Classified} />
+            <Tab.Screen name="Inventories" component={Inventories} />
           </Tab.Navigator>
         );
       }
